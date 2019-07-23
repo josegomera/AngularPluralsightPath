83998bb39322b38eb3262ae76b9100979b75d374
José Gomera <j.gomera@motorcredito.com.do>
2019-07-23 15:59:33 -0400

    update branch

--- src/app/user-settings-form/user-settings-form.component.ts
+++ src/app/user-settings-form/user-settings-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserSettings } from '../model/user-settings';
 
 @Component({
   selector: 'app-user-settings-form',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserSettingsFormComponent implements OnInit {
 
+  userSettings: UserSettings = {
+    name: 'Jose Gomera',
+    emailOffers: true,
+    interfaceStyle: 'dark',
+    subscriptionType: 'Annual',
+    notes: 'here are some notes'
+  };
   constructor() { }
 
   ngOnInit() {
+    console.log(this.userSettings);
   }
 
 }
